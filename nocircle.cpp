3746bdb20bedd6a3cc74986c63a864f6e05f77dd
Wakedd <127494048+Wakedd@users.noreply.github.com>
2023-06-08 14:43:46 +0200

    Update nocircle.cpp

--- nocircle.cpp
+++ nocircle.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 // Global variables
@@ -11,9 +12,11 @@ int g_blurSize = 7;
 int g_fgMaskBlurSize = 13;
 int g_minContourSize = 50;
 int g_maxContourSize = 10000;
-int* value g_aspectRatioThreshold = 0.8;  // Added aspect ratio threshold variable
+int g_aspectRatioThreshold = 80; // Added aspect ratio threshold variable
 
 // Background Subtraction variables
+double g_aspectRatioThresholdDouble = (double)g_aspectRatioThreshold / 100.0;
+
 cv::Ptr<cv::BackgroundSubtractor> g_backgroundSubtractor;
 
 // Function to calculate the aspect ratio of a contour
@@ -28,13 +31,11 @@ double calculateAspectRatio(const std::vector<cv::Point>& contour)
     return aspectRatio;
 }
 
-
 // Function to threshold the image and find contours
 void processImage()
 {
     cv::Mat blurred;
     cv::GaussianBlur(g_frame, blurred, cv::Size(g_blurSize, g_blurSize), 0);
-
     cv::threshold(blurred, g_thresholded, g_thresholdValue, 255, cv::THRESH_BINARY_INV);
 
     // Find contours in the thresholded image
@@ -42,9 +43,8 @@ void processImage()
     cv::findContours(g_thresholded, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
     g_contourImage = cv::Mat::zeros(g_frame.size(), CV_8UC3);
-
-    cv::Scalar contourColor = cv::Scalar(0, 165, 255);  // Orange color (BGR format)
-    cv::Scalar centroidColor = cv::Scalar(0, 0, 255);   // Red color (BGR format)
+    cv::Scalar contourColor = cv::Scalar(0, 165, 255); // Orange color (BGR format)
+    cv::Scalar centroidColor = cv::Scalar(0, 0, 255); // Red color (BGR format)
 
     cv::cvtColor(g_frame, g_outlinesimage, cv::COLOR_GRAY2BGR);
 
@@ -52,8 +52,9 @@ void processImage()
     for (const auto& contour : contours)
     {
         double aspectRatio = calculateAspectRatio(contour);
-        if ((cv::contourArea(contour) > g_minContourSize) && (cv::contourArea(contour) < g_maxContourSize) &&
-            (aspectRatio < g_aspectRatioThreshold))  // Check aspect ratio
+        if ((cv::contourArea(contour) > g_minContourSize) &&
+            (cv::contourArea(contour) < g_maxContourSize) &&
+            (aspectRatio < g_aspectRatioThreshold) ) // Check aspect ratio
         {
             cv::Scalar color = cv::Scalar(rand() % 256, rand() % 256, rand() % 256);
             cv::drawContours(g_contourImage, std::vector<std::vector<cv::Point>>{contour}, -1, color, cv::FILLED);
@@ -90,12 +91,9 @@ void onBlurSizeChange(int, void*)
 {
     if (g_blurSize % 2 == 0)
         ++g_blurSize;
-
     if (g_blurSize < 3)
         g_blurSize = 3;
-
     cv::setTrackbarPos("Blur Size", "Segmented Image", g_blurSize);
-
     processImage();
 }
 
@@ -104,12 +102,9 @@ void onFgMaskBlurSizeChange(int, void*)
 {
     if (g_fgMaskBlurSize % 2 == 0)
         ++g_fgMaskBlurSize;
-
     if (g_fgMaskBlurSize < 3)
         g_fgMaskBlurSize = 3;
-
     cv::setTrackbarPos("Foreground Mask Blur Size", "Segmented Image", g_fgMaskBlurSize);
-
     processImage();
 }
 
@@ -131,12 +126,10 @@ void onAspectRatioThresholdChange(int, void*)
     processImage();
 }
 
-
 int main()
 {
     g_backgroundSubtractor = cv::createBackgroundSubtractorMOG2();
     cv::VideoCapture video("./peak_procedure.mov");
-
     if (!video.isOpened())
     {
         std::cout << "Error opening video file!" << std::endl;
@@ -151,9 +144,7 @@ int main()
     cv::createTrackbar("Foreground Mask Blur Size", "Segmented Image", &g_fgMaskBlurSize, 15, onFgMaskBlurSizeChange);
     cv::createTrackbar("Minimum Contour Size", "Segmented Image", &g_minContourSize, 500, onMinContourSizeChange);
     cv::createTrackbar("Maximum Contour Size", "Segmented Image", &g_maxContourSize, 40000, onMaxContourSizeChange);
-    cv::createTrackbar("Aspect Ratio Threshold", "Segmented Image", &g_aspectRatioThreshold, 1.2, onAspectRatioThresholdChange);
-
-    g_backgroundSubtractor = cv::createBackgroundSubtractorMOG2();
+    cv::createTrackbar("Aspect Ratio Threshold", "Segmented Image", &g_aspectRatioThreshold, 120, onAspectRatioThresholdChange);
 
     while (true)
     {
@@ -161,11 +152,9 @@ int main()
             break;
 
         cv::cvtColor(g_frame, g_frame, cv::COLOR_BGR2GRAY);
-
         processImage();
 
         cv::imshow("Video", g_frame);
-
         int key = cv::waitKey(30);
         if (key == 27)
             break;
@@ -173,6 +162,5 @@ int main()
 
     video.release();
     cv::destroyAllWindows();
-
     return 0;
 }
